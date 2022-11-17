82360d448105ede48faea16e9f3e267a0c36f48a
iakovidisioannis <iakovidisioannis@iti.gr>
2022-11-17 12:44:06 +0100

    Added training code

--- src/guided-diffusion-simple/scripts/image_train.py
+++ src/guided-diffusion-simple/scripts/image_train.py
@@ -5,6 +5,7 @@ Train a diffusion model on images.
 import argparse
 
 from guided_diffusion import logger#, dist_util
+from guided_diffusion.image_datasets import load_data
 from guided_diffusion.resample import create_named_schedule_sampler
 from guided_diffusion.script_util import (
     model_and_diffusion_defaults,
@@ -12,6 +13,7 @@ from guided_diffusion.script_util import (
     args_to_dict,
     add_dict_to_argparser,
 )
+from guided_diffusion.train_util import TrainLoop
 
 
 def main():
@@ -26,6 +28,33 @@ def main():
     )
     # model.to(dist_util.dev())
     schedule_sampler = create_named_schedule_sampler(args.schedule_sampler, diffusion)
+
+    logger.log("creating data loader...")
+    data = load_data(
+        data_dir=args.data_dir,
+        batch_size=args.batch_size,
+        image_size=args.image_size,
+        class_cond=args.class_cond,
+    )
+
+    logger.log("training...")
+    TrainLoop(
+        model=model,
+        diffusion=diffusion,
+        data=data,
+        batch_size=args.batch_size,
+        microbatch=args.microbatch,
+        lr=args.lr,
+        ema_rate=args.ema_rate,
+        log_interval=args.log_interval,
+        save_interval=args.save_interval,
+        resume_checkpoint=args.resume_checkpoint,
+        use_fp16=args.use_fp16,
+        fp16_scale_growth=args.fp16_scale_growth,
+        schedule_sampler=schedule_sampler,
+        weight_decay=args.weight_decay,
+        lr_anneal_steps=args.lr_anneal_steps,
+    ).run_loop()
 
 
 def create_argparser():
