63d9df074862106552b4fc2b0d36e188a3154f4f
iakovidisioannis <iakovidisioannis@iti.gr>
2022-11-22 10:26:07 +0100

    Changed directory name

--- src/guided-diffusion/setup.py
+++ src/guided-diffusion/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="guided-diffusion",
-    py_modules=["guided_diffusion"],
+    py_modules=["guided-diffusion"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
